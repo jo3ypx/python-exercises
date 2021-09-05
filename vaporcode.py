99aa832b6cf21feada684a5c943350e83ba2bcc0
jo3ypx <84823580+jo3ypx@users.noreply.github.com>
2021-09-05 17:41:04 +0100

    Delete vaporcode.py

--- vaporcode.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def vaporcode(s):
-    stringUpper = re.sub(r"\s+", "", s).upper()
-    vaporString = "  ".join(stringUpper)
-    return vaporString
-
-print(vaporcode("Why isn't my code working?"))
